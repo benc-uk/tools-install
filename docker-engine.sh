41304e74a462236cafceb7136833cf9f9ec464e4
Ben Coleman <benc.uk@gmail.com>
2019-06-17 09:39:15 +0100

    Update for WSL2

--- docker-engine.sh
+++ docker-engine.sh
@@ -2,23 +2,26 @@
 set -e 
 
 echo "Installing Docker CE Engine/server..."
+curl -fsSL https://get.docker.com/ | sh
+sudo groupadd docker
+sudo usermod -aG docker $USER
 
-sudo apt-get update
+# sudo apt-get update
 
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+# sudo apt-get install \
+#     apt-transport-https \
+#     ca-certificates \
+#     curl \
+#     gnupg-agent \
+#     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+# sudo add-apt-repository \
+#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#    $(lsb_release -cs) \
+#    stable"
 
-sudo apt-get update
+# sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+# sudo apt-get install docker-ce docker-ce-cli containerd.io
