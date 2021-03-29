4f3a9973b658406b7bbbb832212aea78f09a7174
Ben Coleman <benc.uk@gmail.com>
2021-03-29 11:09:31 +0100

    Mon Mar 29 11:09:31 BST 2021

--- jdk16.sh
+++ jdk16.sh
@@ -7,6 +7,7 @@ start "Adopt Open JDK 16"
 
 wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
 echo "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/adoptopenjdk.list
+sudo apt-get update
 sudo apt-get install -y adoptopenjdk-16-hotspot
 
 end 'java' '-version'
