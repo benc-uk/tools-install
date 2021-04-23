4a1613c0c80cb92798fb3e70554f7d50ab8507e4
Ben Coleman <benc.uk@gmail.com>
2021-04-23 13:49:05 +0100

    🍏 Fri Apr 23 13:49:05 BST 2021

--- base.sh
+++ base.sh
@@ -6,4 +6,12 @@ set -e
 echo -e "\e[34m»»» 📦 \e[32mInstalling \e[33mBase Core Packages\e[0m ..."
 
 sudo apt-get update -y -qq
-sudo apt-get install -y jq curl wget unzip apt-transport-https lsb-release gnupg 
+sudo apt-get install -y \
+  jq \
+  curl \
+  wget \
+  unzip \
+  apt-transport-https \
+  lsb-release \
+  gnupg \
+  shellcheck
