93c60d261597a8e00bc07c3f767e348b585effec
Stuart Leeks <stuart@leeks.net>
2021-05-20 07:42:53 +0100

    Add sudo to `npm install -g` call

--- yarn.sh
+++ yarn.sh
@@ -6,7 +6,7 @@ NAME="Yarn"
 
 echo -e "\e[34m»»» 📦 \e[32mInstalling \e[33m$NAME\e[0m ..."
 
-npm install -g yarn
+sudo npm install -g yarn
 
 echo -e "\n\e[34m»»» 👲 \e[32mYarn is now managed per-project, see https://yarnpkg.com/getting-started/install#per-project-install"
 echo -e "\n\e[34m»»» 💾 \e[32mInstalled to: \e[33m$(which $CMD)"
