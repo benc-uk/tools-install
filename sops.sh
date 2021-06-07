a5573b7e69a6430cf2ff0e6aaa0a28cf48135b41
Bart Plasmeijer <bap@sitecore.net>
2021-06-07 11:35:10 +0200

    clean, and fix

--- sops.sh
+++ sops.sh
@@ -10,10 +10,11 @@ VERSION=${1:-"$(get_latest_release mozilla/sops)"}
 INSTALL_DIR=${2:-"$HOME/.local/bin"}
 CMD=sops
 NAME="Mozilla Sops"
-echo "${VERSION}"
+
 curl -sL "https://github.com/mozilla/sops/releases/download/v${VERSION}/sops-v${VERSION}.linux" -o /tmp/sops
 mkdir -p $INSTALL_DIR
-sudo mv /tmp/sops $INSTALL_DIR
+chmod +x /tmp/sops 
+mv /tmp/sops $INSTALL_DIR
 
 echo -e "\n\e[34m»»» 💾 \e[32mInstalled to: \e[33m$(which $CMD)"
-echo -e "\e[34m»»» 💡 \e[32mVersion details: \e[39m$($CMD version)"
+echo -e "\e[34m»»» 💡 \e[32mVersion details: \e[39m$($CMD --version)"
