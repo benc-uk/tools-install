0a7fe54a6c32e1f068b6f0e5960ff7091d622ae1
Ben Coleman <benc.uk@gmail.com>
2022-06-08 17:37:39 +0100

    Moby fixes

--- moby.sh
+++ moby.sh
@@ -6,8 +6,7 @@ NAME="Moby - Docker CLI & Engine"
 
 echo -e "\e[34m»»» 📦 \e[32mInstalling \e[35m\e[33m$NAME\e[0m ..."
 
-# Weird thing - Microsoft maintains these Moby packages in their own repo
-# Download the Microsoft repository GPG keys
+# Weird thing - Microsoft maintains the Moby packages in their own repo
 wget -q https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
 # Register the Microsoft repository GPG keys
 sudo dpkg -i /tmp/packages-microsoft-prod.deb
@@ -24,10 +23,10 @@ sudo usermod -aG docker "$USER"
 sudo curl -sSL https://raw.githubusercontent.com/moby/moby/master/contrib/init/sysvinit-debian/docker -o /etc/init.d/docker
 sudo chmod +x /etc/init.d/docker
 
-# Start the service
-sudo service docker start
-
 echo -e "\n\e[34m»»» 💾 \e[32mInstalled to: \e[33m$(which $CMD)"
 echo -e "\e[34m»»» 💡 \e[32mVersion details: \e[39m$($CMD --version)"
 echo -e "\e[34m»»» 📜 \e[32mNOTE 1: Please close this shell and open a new one to run docker without sudo"
 echo -e "\e[34m»»» 📜 \e[32mNOTE 2: Docker will NOT auto start on WLS, you must run 'sudo service docker start'"
+
+# Start the service
+sudo service docker start
