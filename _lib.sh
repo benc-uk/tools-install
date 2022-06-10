9b60665467b7790f6a65c0d3941b18fb5f7b80ca
Ben Coleman <benc.uk@gmail.com>
2022-06-10 14:14:51 +0100

    update lib

--- _lib.sh
+++ _lib.sh
@@ -20,8 +20,6 @@ pre_run() {
     echo -e "\e[34m»»» 📢 \e[31mNOTE! Amend your shell startup scripts to make this change permanent:\n\t\e[37mexport PATH=\$PATH:$INSTALL_DIR"
     export PATH="$PATH:$INSTALL_DIR"
   fi
-
-  
 }
 
 post_run() {
@@ -30,4 +28,8 @@ post_run() {
   echo -e "\n\e[34m»»» 💾 \e[32mInstalled to: \e[33m$(which $CMD)"
   # shellcheck disable=all
   echo -e "\e[34m»»» 💡 \e[32mVersion details: \e[39m$($CMD $VERFLAG)"
+}
+
+apt_version() {
+  apt-cache policy "$1" | grep Candidate: | cut -b 14-99
 }
