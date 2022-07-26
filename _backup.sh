8fde117ea764742d0b3fcb3143fbabf539648b95
Ben Coleman <benc.uk@gmail.com>
2022-07-26 14:53:18 +0100

    🥝 Tue Jul 26 14:53:18 BST 2022

--- _backup.sh
+++ _backup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cd $HOME/tools
+DIR="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
+source "$DIR"/_lib.sh
+cd "$DIR" || exit
+
+echo -e "\e[34m»»» 📦 \e[32mBacking up repo to GitHub\e[0m"
 
 git add .
-git commit -m "🍏 `date`"
+git commit -m "$(random_fruit) $(date)"
 git push
