b01976a67697c539659e41e10d900bb6ccc27d57
Ben Coleman <benc.uk@gmail.com>
2022-08-08 14:07:42 +0100

    🥝 Mon Aug  8 14:07:42 BST 2022

--- kubectl.sh
+++ kubectl.sh
@@ -3,13 +3,14 @@ set -e
 source <(curl -sSL benco.io/lib) # Load libary from remote URL, it's safe!
 
 VERSION=${1:-"$(curl -L -s https://dl.k8s.io/release/stable.txt)"}
+VERSION=${VERSION:1} # Remove the leading 'v'
 INSTALL_DIR=${2:-"$HOME/.local/bin"}
 CMD=kubectl
 NAME="Kubectl"
 
 pre_run
 
-curl -sSL "https://dl.k8s.io/release/${VERSION}/bin/linux/amd64/kubectl" -o "$INSTALL_DIR"/kubectl
+curl -sSL "https://dl.k8s.io/release/v${VERSION}/bin/linux/amd64/kubectl" -o "$INSTALL_DIR"/kubectl
 chmod +x "$INSTALL_DIR"/kubectl
 
 post_run version
