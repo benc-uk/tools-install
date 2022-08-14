c6a8023d056c5e5205e71de5b0dc9488c68a6ca0
Ben Coleman <benc.uk@gmail.com>
2022-08-14 15:53:26 +0100

    Add air

---
+++ air.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+set -e
+source <(curl -sSL benco.io/lib) # Load libary from remote URL, it's safe!
+
+GITHUB="cosmtrek/air"
+VERSION=${1:-"$(get_latest_release $GITHUB)"}
+INSTALL_DIR=${2:-"$HOME/.local/bin"}
+CMD=air
+NAME="Air - Hot reloader for Go"
+
+pre_run
+
+curl -sSL https://github.com/$GITHUB/releases/download/v"${VERSION}"/air_"${VERSION}"_linux_amd64 -o "$INSTALL_DIR"/$CMD
+
+post_run -v
